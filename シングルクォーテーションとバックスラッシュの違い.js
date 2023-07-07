00aafcce170ac10115ee80c6fd70cd70b4598ed9
YusukeYamao <fancky0427@gmail.com>
2023-07-08 06:47:44 +0900

    シングルクォーテーションとバックスラッシュの違い

--- シングルクォーテーションとバックスラッシュの違い.js
+++ シングルクォーテーションとバックスラッシュの違い.js
@@ -1,3 +1,6 @@
-### 下記2つは意味が違う、というより下のコードは動かないので注意
-    fetch(`https://api.github.com/users/${githubName}/repos`)
-    fetch('https://api.github.com/users/${githubName}/repos')
+# バックスラッシュとシングルクォーテーションの違い
+### 下記2つは意味が違う（というより下のコードは動かないので注意）
+```javascript
+- fetch(`https://api.github.com/users/${githubName}/repos`)
+- fetch('https://api.github.com/users/${githubName}/repos')
+```
